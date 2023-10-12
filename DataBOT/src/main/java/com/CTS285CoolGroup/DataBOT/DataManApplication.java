9094c94bd5ae3c928dacfb66be5c7d84e86c227c
Jane Martin <martinh5174@student.faytechcc.edu>
2023-10-12 19:07:41 -0400

    first commit

---
+++ DataBOT/src/main/java/com/CTS285CoolGroup/DataBOT/DataManApplication.java
@@ -0,0 +1,13 @@
+package com.CTS285CoolGroup.DataBOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataManApplication.class, args);
+	}
+
+}
---
+++ DataBOT/src/test/java/com/CTS285CoolGroup/DataBOT/DataManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CTS285CoolGroup.DataBOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

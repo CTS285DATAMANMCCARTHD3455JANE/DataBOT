c27abf6db43ba365e3da6dfe4fac86cbab5919ae
Mccarthd3455 <Mccarthd3455@student.faytechcc.edu>
2023-11-01 12:25:44 -0400

    Working on School Computer?!?!?!

--- DataBOT/src/main/java/com/CTS285CoolGroup/DataBOT/DataManApplication.java
+++ DataBOT/src/main/java/com/CTS285CoolGroup/DataBOT/DataManApplication.java
@@ -2,6 +2,9 @@ package com.CTS285CoolGroup.DataBOT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class DataManApplication {
@@ -9,5 +12,12 @@ public class DataManApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DataManApplication.class, args);
 	}
-
+	@RestController
+	public class HelloController{
+		@GetMapping("/")
+		public String index()
+		{
+			return "Dataman coming soon!";
+		}
+	}
 }
